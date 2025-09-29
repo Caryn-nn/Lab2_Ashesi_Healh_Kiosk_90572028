994fa194c015c77d8b5372d2001b98a539d19ae3
caryn <caryn.awittor@ashesi.edu.gh>
2025-09-29 17:42:15 +0000

    Declaring variables

--- src/HealthKiosk.java
+++ src/HealthKiosk.java
@@ -3,8 +3,33 @@
 // LECTURER : MR. DANIEL ADDO
 // DESCRIPTION: THIS CODE IS TO HELP ASHESI UNIVERSITY'S HEALTH CENTER PILOT A TINY SELF SERVICE INTAKE KIOSK
 
+import java.util.Scanner;
 public class HealthKiosk {
     public static void main(String[] args) {
+
+        // declaring variables
+        char service;
+        char kindOfService;
+        double weight;
+        double height;
+        double bmi;
+        String Category = "";
+        double dosage;
+        int healthMetric;
+        double angle;
+        int tablets;
+        double convertAngle;
+        char randomLetter;
+        int number1;
+        int number2;
+        int number3;
+        int number4;
+        String summary ="";
+        String ID ;
+        String firstName;
+        char baseCode;
+        char shiftedLetter;
+
 
     }
 }
